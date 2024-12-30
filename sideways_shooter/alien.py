7344a7401005dda39201676f65cd2ba0c45a0d09
Pablo Rodriguez Sanz <142803076+pablo727@users.noreply.github.com>
2024-12-30 13:47:50 +0800

    Update alien.py

--- sideways_shooter/alien.py
+++ sideways_shooter/alien.py
@@ -1,16 +1,23 @@
 import pygame
 from pygame.sprite import Sprite
 
-class Alien(Sprite):
+class Alien(pygame.sprite.Sprite):
     """A class to represent a single alien in the fleet."""
 
-    def __init__(self, ss_game):
+    def __init__(self, ss_game, fleet_direction):
         """Initialize the alien and set its starting position."""
         super().__init__()
         self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.fleet_direction = fleet_direction
 
         # Load the alien image and set its rect attribute.
-        self.image = pygame.image.load('images/alien.png')
+        original_image = pygame.image.load('images/alien.png')
+
+        # Scale by factor alien size.
+        self.image = pygame.transform.scale(original_image,
+         (int(original_image.get_width() * 0.75),
+           int(original_image.get_height() * 0.75)))
         self.rect = self.image.get_rect()
 
         # Start each new alien near the top left of the screen.
@@ -19,3 +26,11 @@ class Alien(Sprite):
 
         # Store the alien's exact vertical position.
         self.y = float(self.rect.y)
+
+    def update(self):
+        """Update the alien's position based on fleet direction."""
+        self.rect.x += self.settings.alien_speed * self.fleet_direction
+        
+        # Move the alien vertically based on its fleet direction.
+        self.rect.y += self.speed * self.fleet_direction
+
